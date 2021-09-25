9ceaea5146692d99d210ecdf010d79a4b47894aa
Manali Shetye <manalis826@gmail.com>
2021-09-26 02:27:01 +0530

    added model and router

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ShortURL = require("./models/URLShortenerModel");
+const URLShortenerRoute = require("./routes/URLShortenerRoute");
 
 const app = express();
-app.use(express.static(path.join(__dirname,"static")));
+app.use(express.static(path.join(__dirname, "static")));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //view engine setup
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname,"views"));
-
+app.set("views", path.join(__dirname, "views"));
 
 //DB connection
-mongoose.connect(process.env.DB_HOST,{
+mongoose
+  .connect(process.env.DB_HOST, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>{
+  })
+  .then(() => {
     const port = process.env.PORT || 80;
-    app.listen(port, ()=>{
-        console.log(`server started at port`,port);
-    })
-}).catch((err)=>{
+    app.listen(port, () => {
+      console.log(`server started at port`, port);
+    });
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 
 //rendering index.pug
-app.get('/', (req,res)=>{
-    res.status(200).render('index');
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortURL.find();
+  res.status(200).render("index", { shortUrls });
 });
+
+app.use(URLShortenerRoute);
