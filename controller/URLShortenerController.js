56c6ea602ee7795d9b0c9b8ea3b059ffc59c238f
Manali Shetye <manalis826@gmail.com>
2021-09-26 02:17:52 +0530

    changed urlShortpost

--- controller/URLShortenerController.js
+++ controller/URLShortenerController.js
@@ -2,9 +2,9 @@ const ShortURL = require('../models/URLShortenerModel');
 
 const urlShortpost = async(req, res)=>{
     try{
+        console.log('hello controller');
         await ShortURL.create(req.body);
-        const shortUrls = await ShortURL.find();
-        res.status(200).json({shortUrls});
+        res.status(200).json({msg:'url created'});
     }
     catch(err){
         res.status(400).json({ errorMessage: err.message });
@@ -15,6 +15,7 @@ const urlShortget = async(req, res)=>{
     try{
         let urlShortID = req.params.id;
         const url = await ShortURL.findOne({shortURL:urlShortID});
+        if (url == null) return res.sendStatus(404);
         url.clicks++;
         url.save();
         res.redirect(url.fullURL);
@@ -23,5 +24,6 @@ const urlShortget = async(req, res)=>{
         res.status(400).json({ errorMessage: err.message });
     }
 };
+
 
 module.exports = {urlShortpost, urlShortget};
